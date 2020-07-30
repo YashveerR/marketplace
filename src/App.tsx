f1cfd4c13db05e95af29e7817588d19fcfd9f880
Yashveer Ramparsad <codenameyash@gmail.com>
2020-07-30 22:05:36 +0200

    First commital

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SignUpPage from "../src/components/SignUp";
+import SignInPage from "../src/components/SignIn";
+import PasswordForgetPage from "../src/components/PasswordForget";
+import HomePage from "../src/components/Home";
+import AccountPage from "../src/components/Account";
+import AdminPage from "../src/components/Admin";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import * as ROUTES from "../src/constants/routes";
+import { withAuthentication } from "../src/components/Session";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
+      <Route path={ROUTES.SIGN_IN} component={SignInPage} />
+      <Route path={ROUTES.ACCOUNT} component={AccountPage} />
+      <Route path={ROUTES.ADMIN} component={AdminPage} />
+    </Switch>
+  </BrowserRouter>
+);
 
-export default App;
+export default withAuthentication(App);
